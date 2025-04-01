b434167ffdd243cc65e30805eef20d41033aba5b
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2025-04-01 19:51:08 +0200

    migration to the version 1.9

--- utils/test_utils.py
+++ utils/test_utils.py
@@ -7,7 +7,7 @@ from PyQt6.QtGui import QMovie, QPixmap
 from PyQt6.QtWidgets import QListWidgetItem
 from utils.resource_utils import resource_path
 from utils.display_utils import show_cross  # Now importing from display_utils
-
+ 
 def load_tests(window):
     window.testList.clear()
     window.loadingLabel.show()
@@ -17,14 +17,14 @@ def load_tests(window):
     window.loadingLabel.setMovie(window.loading_movie)
     window.loading_movie.start()
     QTimer.singleShot(1500, lambda: populate_tests(window))
-
+ 
 def populate_tests(window):
     window.loading_movie.stop()
     window.loadingLabel.clear()
-
+ 
     if window.test_directory:
         test_files = [file for file in os.listdir(window.test_directory) if file.endswith(".robot")]
-
+ 
         if not test_files:
             show_cross(window)
             window.label.setStyleSheet("color: #ad402a")
@@ -33,59 +33,63 @@ def populate_tests(window):
                 item = QListWidgetItem(file)
                 item.setCheckState(Qt.CheckState.Unchecked)
                 window.testList.addItem(item)
-
+ 
             window.label.setStyleSheet("color: green")
             check_icon_path = resource_path("images/check.png")
             check_pixmap = QPixmap(check_icon_path).scaled(27, 27, QtCore.Qt.AspectRatioMode.KeepAspectRatio)
             window.loadingLabel.setPixmap(check_pixmap)
             window.loadingLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
+ 
 def run_tests(window):
     if not window.test_directory:
-        window.resultLabel.setText("Please select a folder.")
-        window.resultLabel.setStyleSheet("color: #ad402a")
+        window.resultLabel.setStyleSheet("color: none")
+        window.resultLabel.setText("Veuillez sélectionner un dossier.")
+        window.resultLabel.setStyleSheet("color: #ad402a; font: bold")
         return
-    
+       
     if not window.output_directory:
-        window.resultLabel.setText("Please select a location for the results.")
-        window.resultLabel.setStyleSheet("color: #ad402a") 
-        return 
-    
-    selected_tests = [os.path.join(window.test_directory, window.testList.item(i).text())
-                     for i in range(window.testList.count())
-                     if window.testList.item(i).checkState() == Qt.CheckState.Checked]
-    
-    if not selected_tests:
-        window.resultLabel.setText("Please select at least one test.")
-        window.resultLabel.setStyleSheet("color: #ad402a")
+        window.resultLabel.setStyleSheet("color: none")
+        window.resultLabel.setText("Veuillez sélectionner un emplacement pour les résultats.")
+        window.resultLabel.setStyleSheet("color: #ad402a; font: bold")
         return
-    
+       
+    selected_tests = [os.path.join(window.test_directory, window.testList.item(i).text())
+                        for i in range(window.testList.count())
+                        if window.testList.item(i).checkState() == Qt.CheckState.Checked]
+       
+    if not selected_tests:
+        window.resultLabel.setStyleSheet("color: none")
+        window.resultLabel.setText("Veuillez sélectionner au moins un test.")
+        window.resultLabel.setStyleSheet("color: #ad402a; font: bold")
+        return
+       
     num_processes = window.processInput.text()
-    report_title = "Robot Runner - Tests Autos"
-
-    if num_processes == "1":
+    repport_title = "TEST AUTO - SUITE"
+ 
+    if num_processes == 1:
         command = ["robot", "-d", window.output_directory] + selected_tests            
-    else:
-        command = ["pabot", "--processes", num_processes, "--outputdir", window.output_directory, 
-                  "--reporttitle", report_title] + selected_tests
-    
+        output_path = os.path.join(window.output_directory, "output.xml")
+    else :
+        command = ["pabot", "--processes", num_processes, "--outputdir", window.output_directory, "--reporttitle", repport_title] + selected_tests
     subprocess.run(command, cwd=window.test_directory, capture_output=True, text=True)
-    
+       
     output_path = os.path.join(window.output_directory, "output.xml")
     result = ExecutionResult(output_path)
-     
+         
     if result.suite.statistics.failed >= 1:
-        window.resultLabel.setText(f"TOTAL: {result.suite.statistics.total} | PASS: {result.suite.statistics.passed} | FAIL: {result.suite.statistics.failed}")
-        window.resultLabel.setStyleSheet("color: #ad402a")
+        window.resultLabel.setStyleSheet("color: none")
+        window.resultLabel.setText(f"Total: {result.suite.statistics.total} | Passés: {result.suite.statistics.passed} | Échoués: {result.suite.statistics.failed}")
+        window.resultLabel.setStyleSheet("color: #ad402a; font: bold")
     else:
-        window.resultLabel.setText(f"TOTAL: {result.suite.statistics.total} | PASS: {result.suite.statistics.passed} | FAIL: {result.suite.statistics.failed}")
-        window.resultLabel.setStyleSheet("color: green")
-
+        window.resultLabel.setStyleSheet("color: none")
+        window.resultLabel.setText(f"Total: {result.suite.statistics.total} | Passés: {result.suite.statistics.passed} | Échoués: {result.suite.statistics.failed}")
+        window.resultLabel.setStyleSheet("color: green; font: bold")        
+ 
 def open_report(window):
     report_path = os.path.join(window.output_directory, "report.html")
     if os.path.exists(report_path):
         os.system(f'start "" "{report_path}"')
-
+ 
 def open_log(window):
     log_path = os.path.join(window.output_directory, "log.html")
     if os.path.exists(log_path):
