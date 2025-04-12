24c927f93d2895d9cbce137771398d3eedbe27c6
Ashraf-Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2025-04-12 17:52:37 +0200

    Update the multi process execution

--- utils/test_utils.py
+++ utils/test_utils.py
@@ -68,12 +68,14 @@ def run_tests(window):
     log_title = "AUTOS TESTS - LOG"
  
     if num_processes == 1:
-        command = ["robot", "-d", window.output_directory] + selected_tests            
-        output_path = os.path.join(window.output_directory, "output.xml")
+        if len(selected_tests) == 1 :
+            command = ["robot","-d", window.output_directory, selected_tests]          
+        else:
+            command = ["pabot", "--processes", 1, "--outputdir", window.output_directory, "--reporttitle", repport_title, "--logtitle", log_title] + selected_tests          
     else :
         command = ["pabot", "--processes", num_processes, "--outputdir", window.output_directory, "--reporttitle", repport_title, "--logtitle", log_title] + selected_tests
-    subprocess.run(command, cwd=window.test_directory, capture_output=True, text=True)
-       
+    
+    subprocess.run(command, cwd=window.test_directory, capture_output=True, text=True)   
     output_path = os.path.join(window.output_directory, "output.xml")
     result = ExecutionResult(output_path)
          
