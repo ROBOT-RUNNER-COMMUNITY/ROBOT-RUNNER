15bd918a9a296ef1e5c1e67fc8a58d9350692e49
Ashraf-Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2025-04-27 17:35:27 +0200

    Commit new chages : update setup file for windows installer

--- Setup.iss
+++ Setup.iss
@@ -8,6 +8,7 @@ OutputBaseFilename=RobotTestRunner_Setup
 Compression=lzma
 SolidCompression=yes
 SetupIconFile=./images/Logo_exe_grand.ico
+ArchitecturesInstallIn64BitMode=x64
 
 [Files]
 Source: "dist\RobotTestRunner.exe"; DestDir: "{app}"; Flags: ignoreversion
@@ -22,5 +23,5 @@ Name: "{commondesktop}\Robot Test Runner"; Filename: "{app}\RobotTestRunner.exe"
 
 [Run]
 Filename: "{tmp}\vc_redist.x64.exe"; Parameters: "/install /quiet /norestart"; StatusMsg: "Installing Microsoft Visual C++ Redistributable..."; Flags: waituntilterminated
-Filename: "{tmp}\python-3.10.0-amd64.exe"; Parameters: "/quiet InstallAllUsers=1 PrependPath=1 Include_test=0"; StatusMsg: "Installing Python 3.10..."; Flags: waituntilterminated
+Filename: "{tmp}\python-3.10.0-amd64.exe"; Parameters: "/quiet InstallAllUsers=1 PrependPath=1 Include_test=0"; StatusMsg: "Installing Python 3.10..."; Flags: waituntilterminated skipifdoesntexist
 Filename: "{app}\RobotTestRunner.exe"; Description: "{cm:LaunchProgram,Robot Test Runner}"; Flags: nowait postinstall skipifsilent
--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # Author : Achraf KHABAR
 # Date : 2023-10-01
 # Description : Script de construction pour le projet RobotTestRunner
-# Version : 1.1
+# Version : 1.2
 #####################################################################################
 
 set -x
@@ -25,7 +25,20 @@ else
     SEP=";"
 fi
 
-pyinstaller --noconfirm --onefile --windowed --name RobotTestRunner --add-data "./config.xml;." --add-data "./style/style.qss${SEP}style" --add-data "./images/*${SEP}images" --icon=images/Logo_exe_grand.ico main.py
+# Forcer l'import de tous les modules nécessaires
+pyinstaller --noconfirm --onefile --windowed --name "$EXEC_NAME" \
+    --add-data "./config.xml${SEP}." \
+    --add-data "./style/style.qss${SEP}style" \
+    --add-data "./images/*${SEP}images" \
+    --hidden-import PyQt6 \
+    --hidden-import PyQt6.QtWidgets \
+    --hidden-import PyQt6.QtGui \
+    --hidden-import PyQt6.QtCore \
+    --hidden-import numpy \
+    --hidden-import pandas \
+    --hidden-import matplotlib \
+    --hidden-import robotframework \
+    --icon=images/Logo_exe_grand.ico main.py
 
 if [ -f "dist/$EXEC_NAME" ] || [ -f "dist/$EXEC_NAME.exe" ]; then
     echo "L'exécutable a été généré avec succès dans le dossier 'dist/'"
